55a2c2a6a7ad50a4a6fdb68779d0c0b2ba18d3e5
zzdroide <196064264+zzdroide@users.noreply.github.com>
2025-07-10 10:18:15 -0400

    fix: update omz

--- ansible/molecule/default/files/verify_shell.sh
+++ ansible/molecule/default/files/verify_shell.sh
@@ -6,7 +6,7 @@ user_shell=$(getent passwd "$user" | cut -d: -f7)
 [[ "$user_shell" == /bin/zsh ]]
 
 prompt=$(sudo -u "$user" zsh -i -c 'cd ~ && print -P $PS1')
-expected="\x1B[01;37m${user}\x1B[00m@\x1B[01;32minstance\x1B[00m:\x1B[01;34m~\x1B[00m \x1B[01;32m%\x1B[00m "
+expected="\x1B[01;34m${user}\x1B[00m@\x1B[01;33minstance\x1B[00m:\x1B[01;34m~\x1B[00m \x1B[01;32m%\x1B[00m "
 [[ $(echo -e "$prompt") == $(echo -e "$expected") ]]
 
 path=$(sudo -u "$user" zsh -l -c 'echo $PATH')
