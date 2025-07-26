04b36f8803c51abe14915467c56fc8d468977637
zzdroide <196064264+zzdroide@users.noreply.github.com>
2025-07-26 00:06:35 -0400

    feat: require power button press for ssh login

--- ansible/playbooks/roles/deploy/files/ssh_pam_hook.sh
+++ ansible/playbooks/roles/deploy/files/ssh_pam_hook.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 set -euo pipefail
 
+
+# Require logins to be approved by pressing the power button.
+# Whitelist for users "borg" and "vagrant".
+if [[ "$PAM_TYPE" == "open_session" && "$PAM_USER" != "borg" && "$PAM_USER" != "vagrant" ]]; then
+  echo "Press power button to allow login..."
+  timeout 30s acpi_listen | grep -q --max-count=1 "^button/power "
+  beep -f2093 -l80
+fi
+
+
+# Handle borg lock and verify repo
 if [[ "$PAM_USER" == "borg" ]]; then
   if [[ "$PAM_TYPE" == "open_session" && "$PAM_SERVICE" != "hpnsshd" ]]; then
     # Prevent accidental connections without None cipher
