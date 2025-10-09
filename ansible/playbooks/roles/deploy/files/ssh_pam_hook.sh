fa7d03f70167b9b8890d131184d5732659080042
zzdroide <196064264+zzdroide@users.noreply.github.com>
2025-10-08 22:31:12 -0300

    fix: ssh login with power button

--- ansible/playbooks/roles/deploy/files/ssh_pam_hook.sh
+++ ansible/playbooks/roles/deploy/files/ssh_pam_hook.sh
@@ -5,9 +5,18 @@ set -euo pipefail
 # Require logins to be approved by pressing the power button.
 # Whitelist for users "borg" and "vagrant".
 if [[ "$PAM_TYPE" == "open_session" && "$PAM_USER" != "borg" && "$PAM_USER" != "vagrant" ]]; then
-  echo "Press power button to allow login..."
-  timeout 30s acpi_listen | grep -q --max-count=1 "^button/power "
-  beep -f2093 -l80
+  #   echo "Press power button to approve login..."
+  # The echo in the line above is useless. Stdout is only sent to client after this script exits.
+  # Alternative:
+  beep -f1397 -l80
+
+  if acpi_listen -t30 -c1 | grep -q "^button/power "; then
+    beep -f2093 -l80
+  else
+    beep -f175 -l80
+    echo "Did not press power button to approve login."
+    exit 1
+  fi
 fi
 
 
