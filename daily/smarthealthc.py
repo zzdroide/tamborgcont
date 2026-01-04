d71152486bb8d8699822be7abe7126f1e2ff1181
zzdroide <196064264+zzdroide@users.noreply.github.com>
2026-01-04 09:31:15 -0300

    fix: smarthealthc failure signal

--- daily/smarthealthc.py
+++ daily/smarthealthc.py
@@ -12,39 +12,35 @@ def smarthealthc():
 
     for hc_url, disk in get_config_smarthealthc():
         action, msg = process_disk(disk)
+
+        if action == 'log':
+            logger.warning(msg)
+        elif action == 'fail':
+            logger.error(msg)
+
         hc_ping(f'{hc_url}/{action}', msg)
 
 
 def process_disk(disk: str):
     if not Path(disk).is_block_device():
-        msg = f'Disk {disk} is not present'
-        logger.warning(msg)
-        return '/log', msg
+        return 'log', f'Disk {disk} is not present'
 
     try:
         info = sh.sudo('/usr/sbin/smartctl', '-HA', '--json=c', disk, _ok_code=range(256))
         data = json.loads(info)
     except Exception as e:
-        msg = f'Failed to get smart info for {disk}: {e}'
-        logger.warning(msg)
-        return '/log', msg
+        return 'log', f'Failed to get smart info for {disk}: {e}'
 
     if data.get('json_format_version') != [1, 0]:
-        msg = "json_format_version doesn't match"
-        logger.warning(msg)
-        return '/log', msg
+        return 'log', "json_format_version doesn't match"
 
     if not data.get('smart_status', {}).get('passed'):
-        msg = f'Drive self-assessment is bad for {disk}'
-        logger.error(msg)
-        return '/fail', msg
+        return 'fail', f'Drive self-assessment is bad for {disk}'
 
     # https://www.backblaze.com/blog/what-smart-stats-indicate-hard-drive-failures/
     backblaze_attrs = {5, 187, 188, 197, 198}
     for attr in data.get('ata_smart_attributes', {}).get('table', []):
         if attr.get('id') in backblaze_attrs and attr.get('raw', {}).get('value', 0) != 0:
-            msg = f"Attribute {attr['id']} is {attr['raw']['value']} (greater than 0)"
-            logger.error(msg)
-            return '/fail', msg
+            return 'fail', f"Attribute {attr['id']} is {attr['raw']['value']} (greater than 0) for {disk}"
 
     return '0', None  # Success
